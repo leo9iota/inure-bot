9d9d6cf121e360a690a0331f6d9c66b77fcf5591
leo9iota <leo9iota@gmail.com>
2025-05-19 15:40:13 +0200

    Update package list and fix app import path

--- backend/run.py
+++ backend/run.py
@@ -7,7 +7,7 @@ load_dotenv()
 def main() -> None:
     """Run the application with Uvicorn server."""
     uvicorn.run(
-        app="app.main:app",
+        app="main:app",
         host="0.0.0.0",
         port=8000,
         reload=True
